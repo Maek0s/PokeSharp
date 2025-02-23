7aa2be10c49b04f2711d6401aa929b16270f5bc1
Maek0s <maek0szaho@gmail.com>
2025-02-23 01:41:35 +0100

    Player can run, hitboxes fixed and y sorted implemented

--- pokesharp/scripts/MainCharacter.cs
+++ pokesharp/scripts/MainCharacter.cs
@@ -1,31 +1,95 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using System.Numerics;
 
 public partial class MainCharacter : CharacterBody2D
 {
-	public float Speed = 300.0f;
+    // posibilidad de presionar el "Shift" para correr
+    private int max_speed = 110;
+    private AnimatedSprite2D animatedSprite2D;
+    private Godot.Vector2 last_direction = new Godot.Vector2(1,0);
 
-    private Vector2 _velocity = Vector2.Zero;
+    private Godot.Vector2 input_dir = new Godot.Vector2();
+    private bool is_running = false;
+
+    public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// Reinicia la velocidad
-        _velocity = Vector2.Zero;
+        Godot.Vector2 raw_direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        Godot.Vector2 direction = Godot.Vector2.Zero;
 
-        // Movimiento basado en la entrada del usuario
-        if (Input.IsActionPressed("ui_right"))
-            _velocity.X += 1;
-        if (Input.IsActionPressed("ui_left"))
-            _velocity.X -= 1;
-        if (Input.IsActionPressed("ui_down"))
-            _velocity.Y += 1;
-        if (Input.IsActionPressed("ui_up"))
-            _velocity.Y -= 1;
+        // Se prioriza la dirección horizontal sobre la vertical
+        if (raw_direction.X != 0) {
+            direction = new Godot.Vector2(raw_direction.X, 0);
+        } else if (raw_direction.Y != 0) {
+            direction = new Godot.Vector2(0, raw_direction.Y);
+        }
 
-        // Normaliza para evitar velocidad diagonal mayor y aplica la velocidad
-        _velocity = _velocity.Normalized() * Speed;
-
-		Velocity = _velocity;
+        // Ajusta la velocidad dependiendo de si está corriendo o no
+        if (Input.IsActionPressed("run")) {
+            is_running = true;
+            max_speed = 160;
+        } else {
+            max_speed = 110;
+            is_running = false;
+        }
+        
+        Velocity = direction * max_speed;
+        
         MoveAndSlide();
-	}
+
+        // Animaciones
+        if (direction.Length() > 0) {
+            last_direction = direction;
+            play_walk_animation(direction);
+        } else {
+            play_idle_animation(last_direction);
+        }
+
+    }
+
+    private void play_walk_animation(Godot.Vector2 direction) {
+        if (direction.X > 0) {
+            if (is_running) {
+                animatedSprite2D.Play("run_right");
+            } else {
+                animatedSprite2D.Play("walk_right");
+            }
+        } else if (direction.X < 0) {
+            if (is_running) {
+                animatedSprite2D.Play("run_left");
+            } else {
+                animatedSprite2D.Play("walk_left");
+            }
+        } else if (direction.Y > 0) {
+            if (is_running) {
+                animatedSprite2D.Play("run_down");
+            } else {
+                animatedSprite2D.Play("walk_down");
+            }
+        } else if (direction.Y < 0) {
+            if (is_running) {
+                animatedSprite2D.Play("run_up");
+            } else {
+                animatedSprite2D.Play("walk_up");
+            }
+        }
+    }
+
+    private void play_idle_animation(Godot.Vector2 direction) {
+        if (direction.X > 0) {
+            animatedSprite2D.Play("idle_right");
+        } else if (direction.X < 0) {
+            animatedSprite2D.Play("idle_left");
+        } else if (direction.Y > 0) {
+            animatedSprite2D.Play("idle_down");
+        } else if (direction.Y < 0) {
+            animatedSprite2D.Play("idle_up");
+        }
+    }
 }
